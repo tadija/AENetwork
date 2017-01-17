10cad45601d7874a9365cbad137955744b7ca69b
Marko Tadić <tadija@me.com>
2017-01-17 20:29:00 +0100

    Added unit test for `fetchDictionary`

--- Tests/NetworkTests/NetworkTests.swift
+++ Tests/NetworkTests/NetworkTests.swift
@@ -3,14 +3,30 @@ import XCTest
 
 class NetworkTests: XCTestCase {
     
-    func testExample() {
-        /// - TODO: Write real tests
-        XCTAssert(true)
+    func testFetchDictionary() {
+        let fetchDictionary = expectation(description: "Fetch Dictionary")
+        
+        guard let url = URL(string: "https://httpbin.org/get")
+        else { return }
+        
+        let request = URLRequest(url: url)
+        Network.shared.fetchDictionary(with: request) { (data) in
+            do {
+                let data = try data()
+                debugPrint(data)
+                XCTAssert(true)
+            } catch {
+                XCTAssert(false)
+            }
+            fetchDictionary.fulfill()
+        }
+        
+        waitForExpectations(timeout: 5, handler: nil)
     }
 
     static var allTests : [(String, (NetworkTests) -> () throws -> Void)] {
         return [
-            ("testExample", testExample),
+            ("testFetchDictionary", testFetchDictionary),
         ]
     }
     

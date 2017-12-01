dea4d8342a531e6fd08ab57018570bd71ccb8d8c
Marko Tadic <tadija@me.com>
2017-12-01 22:45:57 +0100

    Updating Package.swift

--- Package.swift
+++ Package.swift
@@ -1,5 +1,18 @@
+// swift-tools-version:4.0
+
 import PackageDescription
 
 let package = Package(
-    name: "AENetwork"
+    name: "AENetwork",
+    products: [
+        .library(name: "AENetwork", targets: ["AENetwork"])
+    ],
+    dependencies: [],
+    targets: [
+        .target(
+            name: "AENetwork"),
+        .testTarget(
+            name: "AENetworkTests",
+            dependencies: ["AENetwork"])
+    ]
 )

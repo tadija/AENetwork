5c0b0e5078470ce3bce43ce4ba781d46f5680b03
Marko Tadic <tadija@me.com>
2017-12-05 23:27:11 +0100

    Adding initial tests for Downloader

--- Tests/AENetworkTests/DownloaderTests.swift
+++ Tests/AENetworkTests/DownloaderTests.swift
@@ -9,12 +9,54 @@ import XCTest
 
 class DownloaderTests: XCTestCase {
 
+    // MARK: Properties
+
+    let downloader = Downloader()
+
+    // MARK: Setup
+
+    override func setUp() {
+        downloader.delegate = self
+    }
+
     // MARK: Tests
 
-    static var allTests : [(String, (ParametersTests) -> () throws -> Void)] {
-        return [
+    func testStartDownload() {
+        let url = URL(string: "https://httpbin.org/image/png")!
+        downloader.start(with: url)
 
+        XCTAssertNotNil(downloader.task(with: url), "Should be able to find task with url.")
+        XCTAssertEqual(downloader.tasks.count, 1, "Should have 1 download task.")
+    }
+
+    static var allTests : [(String, (DownloaderTests) -> () throws -> Void)] {
+        return [
+            ("testStartDownload", testStartDownload)
         ]
+    }
+
+}
+
+extension DownloaderTests: NetworkDownloaderDelegate {
+
+    func didStartDownloadTask(_ task: URLSessionDownloadTask, sender: Downloader) {
+
+    }
+
+    func didUpdateDownloadTask(_ task: URLSessionDownloadTask, progress: Float, sender: Downloader) {
+
+    }
+
+    func didStopDownloadTask(_ task: URLSessionDownloadTask, sender: Downloader) {
+
+    }
+
+    func didFinishDownloadTask(_ task: URLSessionDownloadTask, to location: URL, sender: Downloader) {
+
+    }
+
+    func didFailDownloadTask(_ task: URLSessionTask, with error: Error?, sender: Downloader) {
+
     }
 
 }

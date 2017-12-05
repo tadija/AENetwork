fa48e730327a3d384bfffabfb9feb57abb8a38b2
Marko Tadic <tadija@me.com>
2017-12-05 22:36:36 +0100

    Added sources for Network and Downloader unit tests

---
+++ Tests/AENetworkTests/DownloaderTests.swift
@@ -0,0 +1,20 @@
+/**
+ *  https://github.com/tadija/AENetwork
+ *  Copyright (c) Marko Tadić 2017
+ *  Licensed under the MIT license. See LICENSE file.
+ */
+
+import XCTest
+@testable import AENetwork
+
+class DownloaderTests: XCTestCase {
+
+    // MARK: Tests
+
+    static var allTests : [(String, (ParametersTests) -> () throws -> Void)] {
+        return [
+
+        ]
+    }
+
+}

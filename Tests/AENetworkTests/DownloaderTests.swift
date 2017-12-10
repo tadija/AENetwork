2907d2534c4eb83196c3baf0d1ebe7260cc6b4e1
Marko Tadic <tadija@me.com>
2017-12-10 14:34:45 +0100

    Minor improvements and adding more tests

--- Tests/AENetworkTests/DownloaderTests.swift
+++ Tests/AENetworkTests/DownloaderTests.swift
@@ -9,9 +9,18 @@ import XCTest
 
 class DownloaderTests: XCTestCase {
 
+    // MARK: Types
+
+    struct Item: Downloadable {
+        let downloadURL: URL?
+    }
+
     // MARK: Properties
 
     let downloader = Downloader()
+
+    let url1 = URL(string: "https://httpbin.org/image/png")!
+    let url2 = URL(string: "https://httpbin.org/image/jpeg")!
 
     // MARK: Setup
 
@@ -21,17 +30,51 @@ class DownloaderTests: XCTestCase {
 
     // MARK: Tests
 
-    func testStartDownload() {
-        let url = URL(string: "https://httpbin.org/image/png")!
-        downloader.start(with: url)
-
-        XCTAssertNotNil(downloader.task(with: url), "Should be able to find task with url.")
+    func testStartAndStopDownloadWithURL() {
+        downloader.start(with: url1)
+        XCTAssertNotNil(downloader.task(with: url1), "Should be able to find task with url.")
         XCTAssertEqual(downloader.tasks.count, 1, "Should have 1 download task.")
+
+        downloader.start(with: url2)
+        XCTAssertNotNil(downloader.task(with: url2), "Should be able to find task with url.")
+        XCTAssertEqual(downloader.tasks.count, 2, "Should have 2 download tasks.")
+
+        downloader.stop(for: url1)
+        XCTAssertNil(downloader.task(with: url1), "Should not be able to find task with url.")
+        XCTAssertEqual(downloader.tasks.count, 1, "Should have 1 download task.")
+
+        downloader.stop(for: url2)
+        XCTAssertNil(downloader.task(with: url2), "Should not be able to find task with url.")
+        XCTAssertEqual(downloader.tasks.count, 0, "Should have 0 download tasks.")
+    }
+
+    func testStartAndStopDownloadWithItem() {
+        let item1 = Item(downloadURL: url1)
+        let item2 = Item(downloadURL: url2)
+
+        let downloader = Downloader.shared
+
+        item1.startDownload()
+        XCTAssertNotNil(downloader.item(with: url1), "Should be able to find item with url.")
+        XCTAssertEqual(downloader.items.count, 1, "Should have 1 download item.")
+
+        item2.startDownload()
+        XCTAssertNotNil(downloader.item(with: url2), "Should be able to find item with url.")
+        XCTAssertEqual(downloader.items.count, 2, "Should have 2 download items.")
+
+        item1.stopDownload()
+        XCTAssertNil(downloader.item(with: url1), "Should not be able to find item with url.")
+        XCTAssertEqual(downloader.items.count, 1, "Should have 1 download item.")
+
+        item2.stopDownload()
+        XCTAssertNil(downloader.item(with: url2), "Should not be able to find item with url.")
+        XCTAssertEqual(downloader.items.count, 0, "Should have 0 download items.")
     }
 
     static var allTests : [(String, (DownloaderTests) -> () throws -> Void)] {
         return [
-            ("testStartDownload", testStartDownload)
+            ("testStartAndStopDownloadWithURL", testStartAndStopDownloadWithURL),
+            ("testStartAndStopDownloadWithItem", testStartAndStopDownloadWithItem)
         ]
     }
 

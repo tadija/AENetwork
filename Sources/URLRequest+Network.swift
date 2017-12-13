86f1ada1e3e11aaa443b2d143918075d583b1a10
Marko Tadic <tadija@me.com>
2017-12-13 21:03:21 +0100

    Implementing initial convenience initializer for URLRequest

--- Sources/URLRequest+Network.swift
+++ Sources/URLRequest+Network.swift
@@ -8,6 +8,30 @@ import Foundation
 
 extension URLRequest {
 
+    // MARK: Types
+
+    public enum Method: String {
+        case get, post, put, delete
+    }
+
+    // MARK: Init
+
+    init(url: URL,
+         method: Method,
+         headers: [String : String]? = nil,
+         urlParameters: [String : String]? = nil,
+         body: Any? = nil)
+    {
+        if let urlParameters = urlParameters, let urlWithParameters = url.addingParameters(urlParameters) {
+            self.init(url: urlWithParameters)
+        } else {
+            self.init(url: url)
+        }
+        httpMethod = method.rawValue.capitalized
+        allHTTPHeaderFields = headers
+        httpBody = httpBody(with: body)
+    }
+
     // MARK: API
 
     public func fetchData(with network: Network = .shared, completion: @escaping Completion.ThrowableData) {
@@ -20,6 +44,18 @@ extension URLRequest {
 
     public func fetchArray(with network: Network = .shared, completion: @escaping Completion.ThrowableArray) {
         network.fetchArray(with: self, completion: completion)
+    }
+
+    // MARK: Helpers
+
+    private func httpBody(with any: Any?) -> Data? {
+        guard
+            let any = any,
+            let jsonData = try? JSONSerialization.data(withJSONObject: any, options: .prettyPrinted)
+        else {
+            return nil
+        }
+        return jsonData
     }
 
 }

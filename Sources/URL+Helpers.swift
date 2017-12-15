917341e76c0b44cd706a64e1d4b6b02b545698f4
Marko Tadic <tadija@me.com>
2017-12-15 20:54:05 +0100

    Minor improvements

---
+++ Sources/URL+Helpers.swift
@@ -0,0 +1,21 @@
+/**
+ *  https://github.com/tadija/AENetwork
+ *  Copyright (c) Marko Tadić 2017
+ *  Licensed under the MIT license. See LICENSE file.
+ */
+
+import Foundation
+
+private let invalidURL = URL(string: "https://invalid.url")!
+
+extension URL: ExpressibleByStringLiteral {
+    public init(stringLiteral value: StaticString) {
+        self = URL(string: "\(value)") ?? invalidURL
+    }
+}
+
+extension String {
+    var url: URL {
+        return URL(string: self) ?? invalidURL
+    }
+}

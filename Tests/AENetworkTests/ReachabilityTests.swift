f82b82591318883b10fab8f792e58a352ab7d4bd
Mihailo Rancic <mihialo.rancic@gmail.com>
2018-01-09 17:21:38 +0100

    Initial Reachability tests implemented

--- Tests/AENetworkTests/ReachabilityTests.swift
+++ Tests/AENetworkTests/ReachabilityTests.swift
@@ -9,11 +9,46 @@ import XCTest
 
 class ReachabilityTests: XCTestCase {
     
+    // MARK: Properties
+    
+    let reachability = Reachability()
+    
+    // MARK: Lifecycle
+    
+    override func setUp() {
+        super.setUp()
+        
+        reachability.startNotifier()
+    }
+    
+    override func tearDown() {
+        reachability.stopNotifier()
+        
+        super.tearDown()
+    }
+    
     // MARK: Tests
     
-    static var allTests : [(String, (NetworkTests) -> () throws -> Void)] {
+    func testThatBlockIsExecutedWhenNotiferStarts() {
+        let closureExpectation = expectation(description: "listener closure should be executed")
+        reachability.statusDidChange = { (status) in
+            closureExpectation.fulfill()
+        }
+        waitForExpectations(timeout: 3, handler: nil)
+    }
+    
+    func testThatNotificationIsPostedWhenNotifierStarts() {
+        expectation(forNotification: .reachabilityStatusDidChange, object: nil) { (notification) in
+            XCTAssertNotNil(notification.object as? Reachability.Status, "Should set status from notification.")
+            return true
+        }
+        waitForExpectations(timeout: 3, handler: nil)
+    }
+    
+    static var allTests : [(String, (ReachabilityTests) -> () throws -> Void)] {
         return [
-            
+            ("testThatBlockIsExecutedWhenNotiferStarts", testThatBlockIsExecutedWhenNotiferStarts),
+            ("testThatNotificationIsPostedWhenNotifierStarts", testThatNotificationIsPostedWhenNotifierStarts)
         ]
     }
     

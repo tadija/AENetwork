d1f8b2d2e567a033eed3c5de882f2d17c2343847
Mihailo Rancic <mihialo.rancic@gmail.com>
2018-01-09 11:11:51 +0100

    Added initial source file for tests

---
+++ Tests/AENetworkTests/ReachabilityTests.swift
@@ -0,0 +1,20 @@
+/**
+ *  https://github.com/tadija/AENetwork
+ *  Copyright (c) Mihailo Rančić 2018
+ *  Licensed under the MIT license. See LICENSE file.
+ */
+
+import XCTest
+@testable import AENetwork
+
+class ReachabilityTests: XCTestCase {
+    
+    // MARK: Tests
+    
+    static var allTests : [(String, (NetworkTests) -> () throws -> Void)] {
+        return [
+            
+        ]
+    }
+    
+}

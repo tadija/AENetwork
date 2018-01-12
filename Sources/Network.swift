663984c8ab1bf041a697a3a662abdbfd5142e457
Marko Tadic <tadija@me.com>
2018-01-12 22:09:14 +0100

    Removed custom caching logic from Network
    - Note: In case of need it can be achieved by custom implementation outside of Network,
    which now completely relies on system logic (URLSession, URLRequest.CachePolicy etc).

--- Sources/Network.swift
+++ Sources/Network.swift
@@ -11,12 +11,8 @@ public protocol NetworkDelegate: class {
     func didSendRequest(_ request: URLRequest, sender: Network)
     func interceptResult(_ result: () throws -> Network.FetchResult, from request: URLRequest,
                          completion: @escaping Network.Completion.ThrowableFetchResult, sender: Network)
-    func willProvideResultFromCache(for request: URLRequest, sender: Network)
     func didReceiveResult(_ result: () throws -> Network.FetchResult,
                           from request: URLRequest, sender: Network)
-
-    func isValidCache(_ cache: CachedURLResponse, sender: Network) -> Bool
-    func shouldCacheResponse(from request: URLRequest, sender: Network) -> Bool
 }
 
 public extension NetworkDelegate {
@@ -30,16 +26,8 @@ public extension NetworkDelegate {
             return try result()
         }
     }
-    public func willProvideResultFromCache(for request: URLRequest, sender: Network) {}
     public func didReceiveResult(_ result: () throws -> Network.FetchResult,
                                  from request: URLRequest, sender: Network) {}
-
-    public func isValidCache(_ cache: CachedURLResponse, sender: Network) -> Bool {
-        return false
-    }
-    public func shouldCacheResponse(from request: URLRequest, sender: Network) -> Bool {
-        return false
-    }
 }
 
 open class Network {
@@ -63,19 +51,16 @@ open class Network {
     public let reachability: Reachability
     public let fetcher: Fetcher
     public let downloader: Downloader
-    public let cache: URLCache
 
     // MARK: Init
     
     public init(reachability: Reachability = .shared,
                 fetcher: Fetcher = .shared,
-                downloader: Downloader = .shared,
-                cache: URLCache = .shared)
+                downloader: Downloader = .shared)
     {
         self.reachability = reachability
         self.fetcher = fetcher
         self.downloader = downloader
-        self.cache = cache
     }
 
     // MARK: API
@@ -90,9 +75,12 @@ open class Network {
 
     private func dispatchRequest(_ request: URLRequest,
                                 completionQueue: DispatchQueue? = nil,
-                                completion: @escaping Completion.ThrowableFetchResult) {
+                                completion: @escaping Completion.ThrowableFetchResult)
+    {
         delegate?.didSendRequest(request, sender: self)
+
         performRequest(request) { [weak self] (result) in
+
             if let strongSelf = self {
                 strongSelf.delegate?.didReceiveResult(result, from: request, sender: strongSelf)
             }
@@ -124,59 +112,20 @@ open class Network {
         do {
             let modifiedRequest = try delegate?.interceptRequest(request, sender: self)
             let finalRequest = modifiedRequest ?? request
-            provideResponse(for: finalRequest, completion: completion)
+            
+            fetcher.sendRequest(finalRequest, completion: { [weak self] (result) in
+                if let weakSelf = self, let delegate = weakSelf.delegate {
+                    delegate.interceptResult(result, from: request, completion: completion, sender: weakSelf)
+                } else {
+                    completion {
+                        return try result()
+                    }
+                }
+            })
         } catch {
             completion {
                 throw error
             }
-        }
-    }
-
-    private func provideResponse(for request: URLRequest, completion: @escaping Completion.ThrowableFetchResult) {
-        if let cachedResponse = loadCachedResponse(for: request) {
-            delegate?.willProvideResultFromCache(for: request, sender: self)
-            
-            let httpResponse = cachedResponse.response as! HTTPURLResponse
-            let result = FetchResult(response: httpResponse, data: cachedResponse.data)
-            
-            completion {
-                return result
-            }
-        } else {
-            performNetworkRequest(request, completion: completion)
-        }
-    }
-
-    private func loadCachedResponse(for request: URLRequest) -> CachedURLResponse? {
-        guard
-            let cachedResponse = cache.cachedResponse(for: request),
-            let delegate = delegate, delegate.isValidCache(cachedResponse, sender: self)
-        else {
-            cache.removeCachedResponse(for: request)
-            return nil
-        }
-        return cachedResponse
-    }
-
-    private func performNetworkRequest(_ request: URLRequest, completion: @escaping Completion.ThrowableFetchResult) {
-        fetcher.sendRequest(request, completion: { [weak self] (result) in
-            if let weakSelf = self, let delegate = weakSelf.delegate {
-                weakSelf.tryCachingResult(result, from: request, delegate: delegate)
-                delegate.interceptResult(result, from: request, completion: completion, sender: weakSelf)
-            } else {
-                completion {
-                    return try result()
-                }
-            }
-        })
-    }
-
-    private func tryCachingResult(_ result: () throws -> FetchResult,
-                                  from request: URLRequest,
-                                  delegate: NetworkDelegate) {
-        if delegate.shouldCacheResponse(from: request, sender: self), let result = try? result() {
-            let response = CachedURLResponse(response: result.response, data: result.data, storagePolicy: .allowed)
-            cache.storeCachedResponse(response, for: request)
         }
     }
 
